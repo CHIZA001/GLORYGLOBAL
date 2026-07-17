28905e657842cc90761aa4aea30541df28f311a0
CHIZA <samuelgregory455@Gmail.com>
2026-07-17 09:50:14 +0100

    connected project to MongoDB Atlas

--- server/server.js
+++ server/server.js
@@ -1,11 +1,24 @@
-const express = require('express')
+require('dotenv').config();
+const mongoose = require('mongoose');
+const cors = require('cors');
+const express = require('express');
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(express.json())
+app.use(cors());
 //Home Route
 app.get('/',(req, res) => {
     res.send('Welcome to Glory Global Merchandise Inventory Management System');});
+    mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log('✅ connected to Mongo Atlas');
+        
+    })
+ .catch((err) => {
+    console.error('❌ Database connection failed:', err);
+});
+
 //start server
 app.listen(PORT,() => {
     console.log(`Server is running on http://localhost:${PORT}`);});
