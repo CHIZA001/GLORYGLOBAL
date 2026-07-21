1389148abc855cc093384e7f5225d1290da12cec
CHIZA <samuelgregory455@Gmail.com>
2026-07-21 16:05:33 +0100

    updated controllers

--- server/controllers/staffControllers.js
+++ server/controllers/staffControllers.js
@@ -1,4 +1,3 @@
-const Staff = require("../models/Staff");
 const staff = require("../models/Staff");
 const generateStaffId = require("../utils/generateStaffId");
 const bcrypt = require("bcrypt");
@@ -16,3 +15,26 @@ const createStaff = async (req, res) => {
             meessage:"A staff member with this email already exists"
         });
     }
+    const staffId = await generateStaffId();
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newStaff = new Staff({
+        staffId,
+        fullName,
+        email,
+        phone,
+        role,
+        password: hashedPassword,
+        address,
+        gender
+    });
+    await newStaff.save();
+    res.status(201).json({
+        message: "Staff member created successfully",
+        staff:{
+            staffId: newStaff.staffId,
+            fullName:newStaff. fullName,
+            email: newStaff.email,
+            phone: newStaff.phone,
+            role: newStaff.role,
+        }
+    })
