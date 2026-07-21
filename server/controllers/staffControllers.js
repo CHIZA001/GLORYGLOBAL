aae267a5cc9041ac840688b8b6d9592f9080038f
CHIZA <samuelgregory455@Gmail.com>
2026-07-21 11:52:35 +0100

    addition on the controlls

--- server/controllers/staffControllers.js
+++ server/controllers/staffControllers.js
@@ -1,3 +1,4 @@
+const Staff = require("../models/Staff");
 const staff = require("../models/Staff");
 const generateStaffId = require("../utils/generateStaffId");
 const bcrypt = require("bcrypt");
@@ -6,5 +7,12 @@ const createStaff = async (req, res) => {
     if(!fullName || !email || !phone || !password ||!address || !gender){
         return res.status(400).json({
             message: "Please provide all required fields"
+        });
+    }
+    const existingStaff = await Staff.findOne({ email });
+
+    if (existingStaff) {
+        return res.status(400).json({
+            meessage:"A staff member with this email already exists"
         });
     }
