89bda48a7e0c87553953c4a8b61a13d6cedadb1a
CHIZA <samuelgregory455@Gmail.com>
2026-09-04 13:34:55 +0100

    update

--- server/models/Product.js
+++ server/models/Product.js
@@ -8,56 +8,63 @@ const productSchema = new mongoose.Schema(
             unique: true,
             trim: true
         },
-        ProductName: {
-            type: String,
-            required: true,
-            trim:true
-        },
-        category:{
+
+        productName: {
             type: String,
             required: true,
             trim: true
         },
+
+        category: {
+            type: String,
+            required: true,
+            trim: true
+        },
+
         supplier: {
             type: String,
-            required:true,
+            required: true,
             trim: true
         },
+
         costPrice: {
             type: Number,
-            required:true,
-            min:0
-        },
-        sellingPrice: {
-            typew: Number,
             required: true,
             min: 0
         },
-        quantity:{
+
+        sellingPrice: {
+            type: Number,
+            required: true,
+            min: 0
+        },
+
+        quantity: {
             type: Number,
             default: 0,
             min: 0
         },
-        lowStockLevel:{
+
+        lowStockLevel: {
             type: Number,
             default: 5,
             min: 0
         },
+
         description: {
             type: String,
             trim: true
         },
+
         status: {
             type: String,
             enum: ["Active", "Inactive"],
             default: "Active"
-
         }
     },
     {
         timestamps: true
     }
-
-
 );
-module.exports = mongoose.model("Product",productSchema);
+
+module.exports = mongoose.model("Product", productSchema);
--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,7 +1,10 @@
-const express = require(" express");
-const { createProduct } = require("../controller/productController");
-const router = expres.Router();
-
+const express = require("express");
+const { createProduct,
+        getAllproducts
+} = require("../controller/productController");
+const router = express.Router();
 //Create a new Product
 router.post("/", createProduct);
+//Get all Products
+router.get("/", getAllProducts);
 module.exports = router;
